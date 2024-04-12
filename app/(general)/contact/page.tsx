30e9291e84e482bc8dc1bc83cf842b3e4e6d64c4
Developer <developer@MacBook-Pro-de-Developer.local>
2024-04-12 10:41:54 -0600

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+ title: 'SEO Contact',
+ description: 'SEO Contact',
+ keywords: ['Contact Page', 'Loreto', 'Info']
+};
+
+export default function ContactPage(){
+    return (
+        <>
+            <span className="text-7xl">ContactPage</span>
+        </>
+    )
+} 
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+ title: 'SEO Pricing',
+ description: 'SEO Pricing',
+ keywords: ['Pricing Page', 'Loreto', 'Info']
+};
+
+export default function PricingPage(){
+    return (
+        <>
+            <span className="text-7xl">PricingPage</span>
+        </>
+    )
+} 
